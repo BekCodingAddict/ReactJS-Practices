749ad331099dbc7e2612dffacb2108ffc0385640
unknown <experte001@gmail.com>
2024-10-01 15:12:29 +0900

    Creating Store with Redux toolkit

--- 05.BankAccauntTemplate/src/strore-v2.js
+++ 05.BankAccauntTemplate/src/strore-v2.js
@@ -2,9 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import accauntReducer from "./features/accaunts/accauntSlice";
 import customerReducer from "./features/customers/customerSlices";
 
-const store = configureStore({
-  accaunt: accauntReducer,
-  customer: customerReducer,
+configureStore({
+  reducer: {
+    accaunt: accauntReducer,
+    customer: customerReducer,
+  },
 });
-
 export default store;
